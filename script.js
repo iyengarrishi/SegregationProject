ca95487834aec83c3622373b05cbd78218d5e5fc
Rishi <ri2195@columbia.edu>
2014-07-03 16:37:39 -0400

    Added map and fusion layers to page, basic button functionality

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 /**
  * @author Rishi Iyengar
  */
+
+
+function loadMap(){
+	
+	var mapOptions = {
+		center: new google.maps.LatLng(-73.283011,40.801983),
+		zoom:8
+	};
+	
+	var map = new google.maps.Map(document.getElementById("mapDiv"),
+            mapOptions);
+	
+}
+
+
+google.maps.event.addDomListener(window, 'load', loadMap);
