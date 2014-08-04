eed09a1164aaf5b2e3118f5f73caba9d07f5fa71
Rishi <ri2195@columbia.edu>
2014-08-04 18:57:08 -0400

    Changed map layers and enabled slider+button functionality

--- script.js
+++ script.js
@@ -10,114 +10,40 @@
  * 5. Single setMap function at the end, to reload maps each time a button is clicked
  */
 
-/*function addButtons(){
-	
-	$(".btn").on("click", function(){
-		
-		var buttonName = $(this).attr("ref");
-		
-		if(buttonName == "income"){
-			
-			var layer = new google.maps.FusionTablesLayer({
-				query:{
-					select: "col0\x3e\x3e1",
-					from: "1N9cY3w9iT7QjL4ICeg7elwckrYPwyn8x3Y7rXrWb",
-					where: ""
-				}, options: {
-					styleId: 2,
-					templateId: 2
-				}
-			});
-		}else if(buttonName == "housing"){
-			
-			var layer = new google.maps.FusionTablesLayer({
-				query:{
-					select: "col0\x3e\x3e1",
-					from: "1N9cY3w9iT7QjL4ICeg7elwckrYPwyn8x3Y7rXrWb",
-					where: ""
-				}, options: {
-					styleId: 2,
-					templateId: 2
-				}
-			});
-		};
+/*SLIDER STEPS
+ * 1. Create duplicate sliderDiv and slider-values for each button
+ * 2. Copy-paste code and change values
+ * 3. Set null sliders for other two and active slider for current button
+ */
+   
+function initialize (map){
 
-	});
-
-}*/		
-		/*if buttonName = 1 then
-			layer = blah
-		elseif buttonName = 2 then
-			layer = blah2
-			
-		layer.setMap(map)*/   
-
-
-function initialize() {
-
-  var brentwood = new google.maps.LatLng(40.801983, -73.283011);
-
-  var map = new google.maps.Map(document.getElementById('mapDiv'), {
-    center: brentwood,
-    zoom: 8
-  	
-  });
-  
-  var layer = new google.maps.FusionTablesLayer({
-		query: {
-	        select: "col0\x3e\x3e1",
-	        from: "1YsjfaMcjaOlae8N_RF5otqvsb1RlXgpiYdG5TF9w",
-	        where: ""
-      	},
-      	options: {
+  perflayer = new google.maps.FusionTablesLayer({
+    query: {
+        select: "col0\x3e\x3e1",
+        from: "1i8SRdC3GH6IiGRu13FBbHo8hZfTTA0u2ggoYX6Ck",
+        where: ""
+      },
+      options: {
         styleId: 2,
         templateId: 2
-      	}
+      }
   });
-
   
-  $(".btn").on("click", function(){
-		
-		var buttonName = $(this).attr("ref");
-		
-		if(buttonName == "income"){
-			
-			var layer = new google.maps.FusionTablesLayer({
-				query: {
-	        		select: "col0\x3e\x3e1",
-	        		from: "1YsjfaMcjaOlae8N_RF5otqvsb1RlXgpiYdG5TF9w",
-	        		where: ""
-      			},
-      			options: {
-        			styleId: 2,
-        			templateId: 2
-      			}
-  			});
-		}else if(buttonName == "housing"){
-			
-			var layer = new google.maps.FusionTablesLayer({
-				query:{
-					select: "col0\x3e\x3e1",
-					from: "1N9cY3w9iT7QjL4ICeg7elwckrYPwyn8x3Y7rXrWb",
-					where: ""
-				}, options: {
-					styleId: 2,
-					templateId: 2
-				}
-			});
-		};
-		
-		layer.setMap(map);	
-
-	});
-	
-	layer.setMap(map);	
-  
-  }
-  
-
-    /*var layer = new google.maps.FusionTablesLayer({
-   query: {
+  racelayer = new google.maps.FusionTablesLayer({
+    query: {
+        select: "col0\x3e\x3e1",
+        from: "1pTeZhVpf1-xfD_5kefW-KgZ_KFIt1pMkihI3GK7_",
+        where: ""
+      },
+      options: {
+        styleId: 2,
+        templateId: 2
+      }
+  });
+   		
+  houselayer = new google.maps.FusionTablesLayer({
+    query: {
         select: "col0\x3e\x3e1",
         from: "1N9cY3w9iT7QjL4ICeg7elwckrYPwyn8x3Y7rXrWb",
         where: ""
@@ -126,23 +52,98 @@ function initialize() {
         styleId: 2,
         templateId: 2
       }
-
-    
-    
   });
+	
+   perflayer.setMap(map);	
   
-  layer.setMap(map);*/
-  /*var layer2 = new google.maps.FusionTablesLayer({
-    query: {
-      select: 'geometry',
-      from: '1pXvu4vqADERI3wbQjllhbsMEoTkg59rMIvkcpdlJ'
-    }
-   });
-  
-  layer2.setMap(map);
-  
-  }*/
+  }
+ //google.maps.event.addDomListener(window, 'load', initialize);
+ 
+$(function() {
+	
+	var yaphank = new google.maps.LatLng(40.898072, -72.923895);
+	
+	var map = new google.maps.Map(document.getElementById('mapDiv'), {
+	  center: yaphank,
+	  zoom: 9
+    
+  	});
+	
+initialize(map);
+	
+    $('#perfsliderDiv').slider({
+        max: 2011,//slider max value
+        min: 2006,//slider min value
+        step: 1,//slider steps 
+        value: 2006,//start value 
+        slide: function(event, ui) {
+            document.getElementById("perfslider-value").innerHTML = ui.value + "";
+            console.log(ui.value);
+            perflayer.setOptions({
+                query: {
+                    select: "col0\x3e\x3e1",
+                    from: "1i8SRdC3GH6IiGRu13FBbHo8hZfTTA0u2ggoYX6Ck",
+                    where: "ACC_YEAR = " +  ui.value
+                }
+            });
+        }
+    });
+    
+    $('#racesliderDiv').slider({
+        max: 2013,//slider max value
+        min: 2005,//slider min value
+        step: 1,//slider steps 
+        value: 2005,//start value 
+        slide: function(event, ui) {
+            document.getElementById("raceslider-value").innerHTML = ui.value + "";
+            console.log(ui.value);
+            racelayer.setOptions({
+                query: {
+                    select: "col0\x3e\x3e1",
+        			from: "1pTeZhVpf1-xfD_5kefW-KgZ_KFIt1pMkihI3GK7_",
+                    where: "YEAR = " +  ui.value
+                }
+            });
+        }
+    });
+    
+    $(".btn").on("click", function(){
+		
+		var buttonName = $(this).attr("ref");
+		
+		if(buttonName == "perf"){
+			
+			perflayer.setMap(map);
+			racelayer.setMap(null);
+	  		houselayer.setMap(null);
+	  		
+	  		$("#perfsliderDiv").show();
+	  		$("#racesliderDiv").hide();	  		
+  		
+  		}else if(buttonName == "race"){
+		
+			perflayer.setMap(null);
+			racelayer.setMap(map);
+			houselayer.setMap(null);
+			
+			$("#perfsliderDiv").hide();
+	  		$("#racesliderDiv").show();	
+			
+		}else if(buttonName == "house"){
+			
+			perflayer.setMap(null);
+			racelayer.setMap(null);
+			houselayer.setMap(map);
+			
+			$("#perfsliderDiv").hide();
+	  		$("#racesliderDiv").hide();
+			
+		};	
 
-//$(document).ready(addButtons);
+	}); 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+    
+});	
+
+
+	
